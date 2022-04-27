9f70dda490dbfff75d2bb57e54e7469c0f561bd0
Sirbananathe6th <84881706+Sirbananathe6th@users.noreply.github.com>
2022-04-26 19:14:10 -0700

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  enabled: true,
+  mode: 'jit',
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
